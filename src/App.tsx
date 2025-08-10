8497cf988e99494964142525f5be05546a5ae79e
tur4l999 <qarayevtural99@gmail.com>
2025-08-10 17:37:13 +0400

    Create Figma-style Inspect & Auto-Handoff System

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,33 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AppProvider } from './contexts/AppContext';
 import { Header } from './components/layout/Header';
 import { TabBar } from './components/layout/TabBar';
 import { ScreenRenderer } from './components/navigation/ScreenRenderer';
 import { MoreSheet } from './components/sheets/MoreSheet';
+import { InspectPage } from './pages/inspect';
 
 export default function App() {
   return (
-    <AppProvider>
-      <div className="min-h-screen bg-gray-50 text-gray-900">
-        <div className="max-w-md mx-auto min-h-screen relative bg-gray-50">
-          <Header />
-          <ScreenRenderer />
-          <TabBar />
-          <MoreSheet />
+    <Router>
+      <Routes>
+        <Route path="/inspect" element={<InspectPage />} />
+        <Route path="/*" element={
+          <AppProvider>
+            <div className="min-h-screen bg-gray-50 text-gray-900">
+              <div className="max-w-md mx-auto min-h-screen relative bg-gray-50">
+                <Header />
+                <ScreenRenderer />
+                <TabBar />
+                <MoreSheet />
+              </div>
+            </div>
+          </AppProvider>
+        } />
+      </Routes>
+    </Router>
+  );
+}
         </div>
       </div>
     </AppProvider>
